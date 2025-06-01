663fd403567b504029fb2683e36145a49c98c554
DoubleDevT <vetminecrafter@gmail.com>
2025-06-01 04:05:42 +0300

    complete all the tasks from hw check-list

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", () => {
+    const userName = nameInput.value.trim();
+
+    nameOutput.textContent = userName === "" ? "Anonymous" : userName;
+});
